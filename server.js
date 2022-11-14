176fdb61d2c8cf6b2286dd104d3abbe5f662ddbe
jesse-wilson88 <jesse.emmert88@gmail.com>
2022-11-13 22:12:08 -0700

    Just getting things started for the Team Project

---
+++ server.js
@@ -0,0 +1,22 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const mongodb = require("./db/connect");
+
+const port = process.env.PORT || 8080;
+const app = express();
+
+app
+  .use(bodyParser.json())
+  .use((req, res, next) => {
+    next();
+  })
+  .use("/", require("./routes"));
+
+mongodb.initDb((err) => {
+  if (err) {
+    console.log(err);
+  } else {
+    app.listen(port);
+    console.log(`Connected to DB & listening on port ${port}`);
+  }
+});
