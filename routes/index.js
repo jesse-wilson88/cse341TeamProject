e55e51bb7bec93521decf95090576376b413f4c9
jesse-wilson88 <jesse.emmert88@gmail.com>
2022-11-23 19:02:44 -0700

    corrected index routes

--- routes/index.js
+++ routes/index.js
@@ -3,8 +3,8 @@ const router = require("express").Router();
 // router.use("/", require("./books"));
 router.use("/books", require("./books"));
 router.use("/movies", require("./movies"));
-router.use("/music", require("./games"));
-router.use("/games", require("./music"));
+router.use("/music", require("./music"));
+router.use("/games", require("./games"));
 router.use("/api-docs", require("./docs"));
 
 module.exports = router;
